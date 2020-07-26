949fd4c03f784d87f9da36c0b3ce9cbbf3945b8b
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-07-26 13:56:29 +0900

    feat: add main clickEvent

--- app/src/main/java/com/example/pcr/MainActivity.kt
+++ app/src/main/java/com/example/pcr/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.pcr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun clickPCRButton(view: View){
+//        ResultActivity作成
+        val intent = Intent(this,ResultActivity::class.java)
+        startActivity(intent)
     }
 }
